af1670085509a7a57421624f5bfe27f43e13072e
Peter Gemmell <petergemmell65@hotmail.com>
2020-04-20 16:28:24 +0100

    basic python record shop

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/petergemmell/Desktop/G17/codeclan_work/week16/day1/record_shop/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ record_store/inventory/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from inventory.models import *
+
+admin.site.register(Artist)
+admin.site.register(Album)
+
+# Register your models here.
